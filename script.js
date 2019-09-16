c20da785bd2dde25f58c40ce02dee5ca0c93eaa3
Sri Sathwik <sathwik.penjarla@gmail.com>
2019-09-16 18:51:07 +0530

    Bugfix-resolved address disappears: - added logic to display address all the time; - removed createMap global variable.

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-createMap = () => {
-    const element = document.getElementById('map');
-    const options = {
-        center: { lat: 12.935, lng: 77.624 },
-        zoom: 8
-    };
-
-    const map = new google.maps.Map(element, options);
-    google.maps.event.addListener(map, 'click', event => {
-        placeMarker(event.latLng, map);
-    });
+const element = document.getElementById('map');
+const options = {
+    center: { lat: 12.935, lng: 77.624 },
+    zoom: 8
 };
 
+const map = new google.maps.Map(element, options);
+google.maps.event.addListener(map, 'click', event => {
+    placeMarker(event.latLng, map);
+});
+
 const placeMarker = (position, map) => {
+    let isTimeShown = false;
     const { lat, lng } = position;
-    const content = `<b>Lat:</b><span>${lat()}</span> <br />
-    <b>Lng:</b><span>${lng()}</span> <br />`;
-    const contentWithTS = `${content} <b>Marked time:</b>
-    <span>${(new Date()).toLocaleString()}</span> <br />`;
+    const [date, content] = [new Date(), `<b>Lat:</b><span>${lat()}</span> <br />
+    <b>Lng:</b><span>${lng()}</span> <br />`];
 
     const marker = new google.maps.Marker({ position, map });
     const infoWindow = new google.maps.InfoWindow({ content });
     getPostalAddr(map, infoWindow, marker, position, content);
 
     marker.addListener('click', () => {
-        infoWindow.setContent(contentWithTS);
+        const newContent = getInfoWindowContent(isTimeShown, infoWindow.getContent(), date);
+        !isTimeShown && (isTimeShown = true);
+        infoWindow.setContent(newContent);
         infoWindow.open(map, marker);
     });
 };
+
+const getInfoWindowContent = (isTimeShown, content, date) => {
+    if (!isTimeShown) {
+        return `${content} <br /> <b>Marked time:</b>
+            <span>${date.toLocaleString()}</span>`;
+    }
+    return content;
+}
 
 const getPostalAddr = (map, infoWindow, marker, position, content) => {
     const geocoder = new google.maps.Geocoder;
